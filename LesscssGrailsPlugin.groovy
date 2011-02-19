16a614b7722aa1fe7d2f9e9d6adfc156c27d51b9
David Marquis <davidmarquis@MacBookPro.local>
2011-02-19 08:37:00 -0500

    Updated less.js version to 1.0.41 (latest) + removed plugin distribution ZIP (now in main Grails repository)

--- LesscssGrailsPlugin.groovy
+++ LesscssGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class LesscssGrailsPlugin {
     // the plugin version
-    def version = "0.9.3"
+    def version = "0.9.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.2 > *"
     // the other plugins this plugin depends on
--- src/groovy/com/github/grails/lesscss/Constants.groovy
+++ src/groovy/com/github/grails/lesscss/Constants.groovy
@@ -1,13 +1,10 @@
 package com.github.grails.lesscss
 
-/**
- *
- */
 class Constants {
 
     public static final String LESS_EXTENSION = ".less"
 
-    public static final String LESS_SCRIPT_NAME = 'less-1.0.40'
+    public static final String LESS_SCRIPT_NAME = 'less-1.0.41'
     public static final String LESS_SCRIPT_FILE = "${LESS_SCRIPT_NAME}.js"
     public static final String LESS_SCRIPT_LOCATION = "/js/${LESS_SCRIPT_FILE}"
 }
