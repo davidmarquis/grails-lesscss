a9a5cad3c564b4aca2d04a36d1761d182a46d58f
David Marquis <davidmarquis@MacBookPro.local>
2011-08-30 21:25:52 -0400

    Version bump to 1.0.0

--- LesscssGrailsPlugin.groovy
+++ LesscssGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class LesscssGrailsPlugin {
     // the plugin version
-    def version = "0.9.6"
+    def version = "1.0.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.2 > *"
     // the other plugins this plugin depends on
